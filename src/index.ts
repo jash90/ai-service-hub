89732638dbad9365dbdc43efb8db1be963505ae6
bartlomiej.zimny <bartlomiej.zimny@appwise.pl>
2025-02-15 16:47:04 +0100

    Add dotenv configuration to index module

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import dotenv from 'dotenv';
 import deepSeek from "./deepSeek";
 import gemini from "./gemini";
 import lmstudio from "./lmstudio";
 import ollama from "./ollama";
 import openAi from "./openAi";
 import qdrant from "./qdrant";
+dotenv.config();
 
 export { openAi, deepSeek, gemini, lmstudio, ollama, qdrant };
