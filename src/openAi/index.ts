bfd727957716c96706fc86778858007dec2e91df
bartlomiej.zimny <bartlomiej.zimny@appwise.pl>
2025-02-15 17:36:12 +0100

    Add OpenAI config to module exports

--- src/openAi/index.ts
+++ src/openAi/index.ts
@@ -3,7 +3,7 @@ import {chat} from "./chat";
 import {tts} from "./tts";
 import {transcript} from "./transcript";
 import {vision} from "./vision";
-
+import {openai} from "./config";
 
 export default {
     embedding,
@@ -11,4 +11,5 @@ export default {
     tts,
     transcript,
     vision,
+    config : openai
 }
